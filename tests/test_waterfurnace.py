84d54f8e9e2787f8eacce03dee93f6e90cc317f4
Sean Dague <sean@dague.net>
2018-11-11 09:23:23 -0500

    fix unit tests now that cli does something

--- tests/test_waterfurnace.py
+++ tests/test_waterfurnace.py
@@ -31,8 +31,8 @@ def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
     result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'waterfurnace.cli.main' in result.output
+    assert result.exit_code == 2
+    assert 'Usage: main [OPTIONS]' in result.output
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    assert 'Show this message and exit.' in help_result.output
