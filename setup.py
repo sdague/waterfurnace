289595d402016f43eb68f4bf36b40f4d91cb2520
Sean Dague <sean@dague.net>
2026-01-31 07:06:20 -0500

    Bump version: 1.5.0 → 1.5.1

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requirements = [
 
 setup(
     name="waterfurnace",
-    version="1.5.0",
+    version="1.5.1",
     description="Python interface for waterfurnace geothermal systems",
     entry_points={"console_scripts": ["waterfurnace=waterfurnace.cli:main"]},
     long_description=readme + "\n\n" + history,
